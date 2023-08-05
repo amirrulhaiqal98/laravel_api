0b9c66513972ab25703c06c755609cf8979b3181
amirrulhaiqal98 <amirrulhaiqal98@gmail.com>
2023-08-05 18:48:36 +0800

    filtering more data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,23 +8,24 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
-use App\Services\v1\CustomerQuery;
+use App\Filters\V1\CustomersFilter;
 use Illuminate\Http\Request; // Corrected the namespace
 
 class CustomerController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource.s
      */
     public function index(Request $request)
     {
-        $filter = new CustomerQuery();
+        $filter = new CustomersFilter();
         $queryItems = $filter->transform($request); //['column','operator','value']
 
         if (count($queryItems) == 0) {
             return new CustomerCollection(Customer::paginate());
         } else {
-            return new CustomerCollection(Customer::where($queryItems)->paginate());
+            $customers = Customer::where($queryItems)->paginate();
+            return new CustomerCollection($customers ->appends($request->query()));
         }
     }
 
